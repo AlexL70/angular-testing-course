c22d75b0b114731b7d8752007c936aa40799b451
AlexL70 <alexander.levinson.70@gmail.com>
2019-07-08 14:41:49 +0300

    9. Using Dependency Injection in Angular Testing - The Angular TestBed

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -1,12 +1,20 @@
 import { CalculatorService } from './calculator.service';
 import { LoggerService } from './logger.service';
+import { TestBed } from '@angular/core/testing';
+
 describe('Calculator Service', () => {
   let loggerSpy: LoggerService;
   let calculator: CalculatorService;
 
   beforeEach(() => {
     loggerSpy = jasmine.createSpyObj('LoggerService', ['log']);
-    calculator = new CalculatorService(loggerSpy);
+    TestBed.configureTestingModule({
+      providers: [
+        CalculatorService,
+        { provide: LoggerService, useValue: loggerSpy }
+      ]
+    });
+    calculator = TestBed.get(CalculatorService);
   });
 
   it('should add two numbers', () => {
